6b4eabdd8d2e156dc8866d1cae6003709c259c6e
Anuar Imanbayev <anuar.iman@gmail.com>
2015-07-02 22:38:54 -0700

    find-missing-element
    Solution to finding the missing element in shuffled list

---
+++ python/find-missing-element.py
@@ -0,0 +1,13 @@
+"""Solution to finding the missing element in shuffled list"""
+
+def difference_set(orig, shuffled):
+    """finds the missing element using a set."""
+
+    return set(orig).difference(set(shuffled)).pop()
+
+def difference_iterative(orig, shuffled):
+    """finds the missing element by iterating over the list"""
+
+    for x in orig:
+        if not x in shuffled:
+            return x
