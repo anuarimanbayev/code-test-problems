157e3516a6eb427f09412f3736047364dc206914
Anuar Imanbayev <anuar.iman@gmail.com>
2015-07-04 15:34:32 -0700

    9 Dictionaries Adding New Entires

---
+++ codecademy/python/9dic-new-entries.py
@@ -0,0 +1,14 @@
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print(menu['Chicken Alfredo'])
+
+# Your code here: Add some dish-price pairs to menu!
+menu['Besbarmak'] = 20.00
+print(menu['Besbarmak'])
+menu['Plov'] = 30.00
+print(menu['Plov'])
+menu['7 Heavens Cake'] = 99.99
+print(menu['7 Heavens Cake'])
+
+print("There are " + str(len(menu)) + " items on the menu.")
+print(menu)
