ea308c6cd62f3718949b0b2825c38dadc74aa5af
Anuar Imanbayev <anuar.iman@gmail.com>
2015-07-04 15:33:49 -0700

    8 Dictionaries

---
+++ codecademy/python/8dictionaries.py
@@ -0,0 +1,8 @@
+# Assigning a dictionary with three key-value pairs to residents:
+residents = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
+
+print(residents['Puffin'])              # Prints Puffin's room number
+
+# Your code here!
+print(residents['Sloth'] )              # Prints Sloth's room number
+print(residents['Burmese Python'])      # Prints Burmese Python's room number
