4d5182d97fa2dbc74220e3c3431d21d5e2813f04
Anuar Imanbayev <anuar.iman@gmail.com>
2015-07-06 23:59:19 -0700

    1 Battleship Board Program

---
+++ codecademy/python/battleship/1battleship-board.py
@@ -0,0 +1,11 @@
+"""
+BATTLESHIP PROJECT
+In this project you will build a simplified, one-player version of the classic board game Battleship! In this version of the game, there will be a single ship hidden in a random location on a 5x5 grid. The player will have 10 guesses to try to sink the ship.
+
+To build this game we will use our knowledge of lists, conditionals and functions in Python.
+
+The first thing we need to do is to set up the game board.
+"""
+
+# Setting up the empty board
+board = []
