f0a5640ee34fe57893b4b52bbdf70a4bb02ee425
Anuar Imanbayev <anuar.iman@gmail.com>
2015-07-24 12:51:58 -0700

    16 Review Car

---
+++ codecademy/python/classes/16review-car.py
@@ -0,0 +1,17 @@
+# Classes
+# REVIEW
+# Class Basics
+
+"""
+We also use the word object in parentheses because we want our classes to
+inherit the object class. This means that our class has all the properties
+of an object, which is the simplest, most basic class
+"""
+class Car(object):
+    pass
+
+"""
+We can use classes to create new objects,
+which we say are instances of those classes.
+"""
+my_car = Car()
