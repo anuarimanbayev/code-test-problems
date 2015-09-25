0b8f0a39c8cf979b8439fb4792c6ceddc83d028f
Anuar Imanbayev <anuar.iman@gmail.com>
2015-09-24 21:32:02 -0700

    3

---
+++ hackerrank/ruby/5methods/3variablearguments.rb
@@ -0,0 +1,8 @@
+# Your code here
+def full_name(firstName, *middleName, lastName)
+    if middleName.empty?
+        return firstName.to_s + " " + lastName.to_s
+    else
+        return firstName.to_s + " " + (middleName * " ") + " " + lastName.to_s
+    end    
+end
